25b288d0e4437eb2f899dfe4568bbb54fb42ec31
Ivan Katanovic <ivan.katanovic@styria.dev>
2026-01-28 09:53:22 +0100

    Fix index_together

--- daguerre/models.py
+++ daguerre/models.py
@@ -161,7 +161,9 @@ class AdjustedImage(models.Model):
     requested = models.CharField(max_length=100)
 
     class Meta:
-        index_together = [['requested', 'storage_path'], ]
+        indexes = [
+            models.Index(fields=['requested', 'storage_path']),
+        ]
 
     def __str__(self):
         return u"{0}: {1}".format(self.storage_path, self.requested)
